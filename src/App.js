9251220cfa6c973339f9b5453cbf7c32e2f7df1d
Dulatto <dulat.omarov@gmail.com>
2022-10-17 22:11:44 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <div className="container-fluid">
-        <div className="row">
-          <div className="col-12">
+        <div className="row ">
+          <div className="col-12 bg-light pt-2">
             <ul class="nav nav-pills float-end mb-3" id="pills-tab" role="tablist">
               <li class="nav-item" role="presentation">
                 <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
