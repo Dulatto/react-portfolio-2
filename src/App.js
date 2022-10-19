814d303dd47701a80726b054ea0179875a665efd
Dulatto <dulat.omarov@gmail.com>
2022-10-18 22:28:50 -0400

    typed.js

--- src/App.js
+++ src/App.js
@@ -8,28 +8,28 @@ function App() {
       <div className="container-fluid">
         <div className="row ">
           <div className="col-12 bg-light pt-2">
-            <ul class="nav nav-pills float-end mb-3" id="pills-tab" role="tablist">
-              <li class="nav-item" role="presentation">
-                <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
+            <ul className="nav nav-pills float-end mb-3" id="pills-tab" role="tablist">
+              <li className="nav-item" role="presentation">
+                <button className="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
               </li>
-              <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">About</button>
+              <li className="nav-item" role="presentation">
+                <button className="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">About</button>
               </li>
-              <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Portfolio</button>
+              <li className="nav-item" role="presentation">
+                <button className="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Portfolio</button>
               </li>
-              <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contacts</button>
+              <li className="nav-item" role="presentation">
+                <button className="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contacts</button>
               </li>
             </ul>
           </div>
           <div className="col-12 p-0">
-            <div class="tab-content" id="pills-tabContent">
-              <div class="tab-pane fade show home-page active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+            <div className="tab-content" id="pills-tabContent">
+              <div className="tab-pane fade show home-page active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
                 <HomePage />
               </div>
-              <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">222222</div>
-              <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">33333</div>
+              <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">222222</div>
+              <div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">33333</div>
             </div>
           </div>
         </div>
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import Typed from 'typed.js';
 
 function HomePage(props) {
+    const el = React.useRef(null);
+
+    const typed = React.useRef(null);
+    useEffect(() => {
+        const options = {
+            strings: ['Some <i>strings</i> with', 'Some <strong>HTML</strong>', 'Chars &times; &copy;'],
+
+            typeSpeed: 50,
+            backSpeed: 50,
+            fadeOut: true,
+            loop: true
+        };
+
+        // elRef refers to the <span> rendered below
+        typed.current = new Typed(el.current, options);
+
+        return () => {
+            // Make sure to destroy Typed instance during cleanup
+            // to prevent memory leaks
+            typed.current.destroy();
+        }
+    }, [])
+
     return (
-        <div className="">
-            TEST
+        <div className="home-text">
+            <span className='d-inline' style={{ whiteSpace: 'pre' }} ref={el} />
         </div>
     );
 }
