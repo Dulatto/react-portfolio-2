40f58b0e10c824ebd5232b0cac9a8b22a967f70a
Dulatto <dulat.omarov@gmail.com>
2022-10-19 23:24:53 -0400

    new comps

---
+++ src/Components/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AboutPage(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default AboutPage;
---
+++ src/Components/ContactPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ContactPage(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default ContactPage;
