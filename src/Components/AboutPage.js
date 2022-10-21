a1431216d7ef0c72fd98dd9890e735249ac8eb9d
Dulatto <dulat.omarov@gmail.com>
2022-10-20 23:27:19 -0400

    about

--- src/Components/AboutPage.js
+++ src/Components/AboutPage.js
@@ -2,23 +2,22 @@ import React from 'react';
 
 function AboutPage(props) {
     return (
-        <div className="row">
+        <div className="row about-page p-2">
             <div className="col-md-6">
                 <h2>About Me</h2>
                 <div className="section-title">
-                    <div className="row"> <img src="img/about.jpg" className="img-responsive pull-left" alt="" />
-                        <p>Hello World! <br />My name is Dulat. I am a Front-end Developer, graduating of Web technology program in John Abbott college. I have 3 years commercial experience providing front-end development, producing high quality responsive websites and exceptional user experience.<br />
-                            I like to code things from scratch, and enjoy bringing ideas to life in the browser.<br /> By day I'm the developer at a local agency. By night I spend my time working on freelance projects and building my own products.<br />
-                            All my skills you can see in right side for Desktop version or in following section for mobile.</p>
-                        <div className="social">
-                            <ul>
-                                <li><a href="https://www.linkedin.com/in/dulat-omarov/"><i className="fab fa-linkedin"></i></a></li>
-                                <li><a href="https://www.facebook.com/dulat.omarov.3"><i className="fab fa-facebook"></i></a></li>
-                                <li><a href="https://github.com/Dulatto"><i className="fab fa-github"></i></a></li>
+                    <img src="/src/img/about.jpg" width='300' height='250' />
+                    <p>Hello World! <br />My name is Dulat. I am a Front-end Developer, graduating of Web technology program in John Abbott college. I have 3 years commercial experience providing front-end development, producing high quality responsive websites and exceptional user experience.<br />
+                        I like to code things from scratch, and enjoy bringing ideas to life in the browser.<br /> By day I'm the developer at a local agency. By night I spend my time working on freelance projects and building my own products.<br />
+                        All my skills you can see in right side for Desktop version or in following section for mobile.</p>
+                    <div className="social">
+                        <ul>
+                            <li className='d-inline me-3'><a href="https://www.linkedin.com/in/dulat-omarov/"><i className="fab fa-linkedin"></i></a></li>
+                            <li className='d-inline'><a href="https://github.com/Dulatto"><i className="fab fa-github"></i></a></li>
 
-                            </ul>
-                        </div>
+                        </ul>
                     </div>
+
                 </div>
             </div>
             <div className="col-md-6">
@@ -34,7 +33,7 @@ function AboutPage(props) {
                         <div className="box"><span >Bootstrap</span></div>
                         <div className="box"><span >WordPress</span></div>
                         <div className="box"><span >Github</span></div>
-                        <div className="box"><button className="btn btn-default btn-md"><a href="CV_Dulat_Omarov_2022.pdf" ><b> Download CV</b></a></button></div>
+                        <div className="box"><button className="btn btn-default btn-md"><a href="/src/doc/CV_Dulat_Omarov_2022.pdf" ><b> Download CV</b></a></button></div>
                     </div>
                 </figure>
             </div>
