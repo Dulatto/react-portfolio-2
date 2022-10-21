44b3e5d02a5aaa4ecd6db5df4c1e317bb8bef3da
Dulatto <dulat.omarov@gmail.com>
2022-10-20 23:39:42 -0400

    about

--- src/Components/AboutPage.js
+++ src/Components/AboutPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import about from '../img/about.jpg'
 
 function AboutPage(props) {
     return (
@@ -6,7 +7,7 @@ function AboutPage(props) {
             <div className="col-md-6">
                 <h2>About Me</h2>
                 <div className="section-title">
-                    <img src="/src/img/about.jpg" width='300' height='250' />
+                    <img src={about} width='300' height='250' />
                     <p>Hello World! <br />My name is Dulat. I am a Front-end Developer, graduating of Web technology program in John Abbott college. I have 3 years commercial experience providing front-end development, producing high quality responsive websites and exceptional user experience.<br />
                         I like to code things from scratch, and enjoy bringing ideas to life in the browser.<br /> By day I'm the developer at a local agency. By night I spend my time working on freelance projects and building my own products.<br />
                         All my skills you can see in right side for Desktop version or in following section for mobile.</p>
@@ -14,7 +15,6 @@ function AboutPage(props) {
                         <ul>
                             <li className='d-inline me-3'><a href="https://www.linkedin.com/in/dulat-omarov/"><i className="fab fa-linkedin"></i></a></li>
                             <li className='d-inline'><a href="https://github.com/Dulatto"><i className="fab fa-github"></i></a></li>
-
                         </ul>
                     </div>
 
