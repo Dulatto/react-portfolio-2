1be3ea03cb03facf389d7825b7ab7a64ea13528e
Dulatto <dulat.omarov@gmail.com>
2022-10-23 22:33:27 -0400

    portfolio

--- src/Components/PortfolioPage.js
+++ src/Components/PortfolioPage.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function PortfolioPage(props) {
+    const [activeButton, setActiveButton] = useState('all');
+
+    const handleButtonName = (e) => {
+        console.log('handleButtonName', e.target.name);
+        setActiveButton(e.target.name);
+    }
+
     return (
         <div>
             <h4>MY PORTFOLIO</h4>
             <div className="row">
                 <div className="col-12 position-relative">
                     <div className="position-absolute start-50" >
-                        <button type="button" className="btn btn-danger me-3 ">ALL</button>
-                        <button type="button" className="btn btn-warning me-3">WEB </button>
-                        <button type="button" className="btn btn-success">REACT</button>
+                        <button type="button" className={`btn fw-bold me-3 ${activeButton === 'all' ? 'border-bottom' : null}`} name='all' onClick={(e) => handleButtonName(e)}>ALL</button>
+                        <button type="button" className={`btn fw-bold me-3 ${activeButton === 'web' ? 'border-bottom' : null}`} name='web' onClick={(e) => handleButtonName(e)}>WEB </button>
+                        <button type="button" className={`btn fw-bold me-3 ${activeButton === 'react' ? 'border-bottom' : null}`} name='react' onClick={(e) => handleButtonName(e)}>REACT</button>
                     </div>
                 </div>
             </div>
