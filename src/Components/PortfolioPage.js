aa8394c37980f63c111ba81d8bd37964efc63859
Dulatto <dulat.omarov@gmail.com>
2022-10-26 21:39:13 -0400

    portfolio

--- src/Components/PortfolioPage.js
+++ src/Components/PortfolioPage.js
@@ -11,9 +11,8 @@ const images = [
     { id: 8, text: 'Weather Widget', description: 'React, JavaScript', type: 'React', url: 'https://react-weather-widget-dulatto.vercel.app/' },
     { id: 9, text: 'ToDo List', description: 'React, JavaScript', type: 'React', url: 'https://react-todo-list-iiwqmujls.vercel.app/' },
     { id: 10, text: 'Covid Tracker', description: 'React, JavaScript', type: 'React', url: 'https://react-corona-tracker-plum.vercel.app/' },
-    { id: 11, text: 'Portfolio', description: 'React, JavaScript', type: 'JS' },
-    { id: 12, text: 'Shopping cart', description: 'React, Redux, JavaScript', type: 'React', url: 'https://redux-store-orpin.vercel.app/' },
-    { id: 13, text: 'Timer', description: 'React, JavaScript', type: 'React', url: 'https://react-timer-dulatto.vercel.app/' },
+    { id: 11, text: 'Shopping cart', description: 'React, Redux, JavaScript', type: 'React', url: 'https://redux-store-orpin.vercel.app/' },
+    { id: 12, text: 'Timer', description: 'React, JavaScript', type: 'React', url: 'https://react-timer-dulatto.vercel.app/' },
 ]
 
 let list = images.map(item => {
@@ -44,17 +43,19 @@ function PortfolioPage(props) {
         return (
             <div className="col-lg-4 col-sm-6 p-2 portfolio-item" >
                 <div className={`card hover-bg text-dark mb-2 bground-${item.id}`} key={item.id}>
-                    <div className="card-body">
-                        <div class="hover-text">
-                            <div class="overlay-caption">
-                                <div class="overlay-content">
-                                    <h4 className="card-title">{item.text}</h4>
-                                    <hr />
-                                    <h5 className="card-text">{item.description}</h5>
+                    <a href={item.url} target="_blank">
+                        <div className="card-body">
+                            <div class="hover-text">
+                                <div class="overlay-caption">
+                                    <div class="overlay-content">
+                                        <h4 className="card-title">{item.text}</h4>
+                                        <hr />
+                                        <h5 className="card-text">{item.description}</h5>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
             </div >);
     }
